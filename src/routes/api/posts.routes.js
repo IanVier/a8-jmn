a049a9c68a984aa8b4c8a43045bbb6bffbdbdf44
Javier Morales <eyjavi@gmail.com>
2025-11-09 18:22:34 +0100

    elimino lo credo para post para rehacerlo.

--- src/routes/api/posts.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { Posts } from '../../controllers/posts.controller.js'
-
-const router = Router();
-
-router.get('/', Posts.getAll);
-
-export default router; 
