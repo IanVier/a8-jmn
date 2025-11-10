c2c85906bc76bd372d0ad055e81bb442e9b65c76
Javier Morales <eyjavi@gmail.com>
2025-11-10 07:23:57 +0100

    post api works

--- src/models/posts.model.js
+++ src/models/posts.model.js
@@ -5,6 +5,27 @@ const selectPosts = async () => {
     return result;
 };
 
+const selectById = async (postId) => {
+    const [result] = await db.query(`
+        select * from posts where id = ?
+        `, [postId] );
+    if (result.length === 0) return null;
+    return result[0];
+};
+
+const insertPost = async (postData) => {
+    const { titulo, descripcion, autores_id, categoria } = postData;
+    const query = `
+        insert into posts 
+        (titulo, descripcion, autores_id, categoria) 
+        values (?, ?, ?, ?)
+        `;
+    const values = [titulo, descripcion, autores_id, categoria];
+    const [result] = await db.query(query, values);
+    return result.insertId;
+};
+
+
 export const PostsModel = {
-    selectPosts
+    selectPosts, insertPost, selectById
 }
